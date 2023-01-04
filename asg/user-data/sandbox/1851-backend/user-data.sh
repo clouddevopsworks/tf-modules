9393fcde0317984ec57c4175e049c1a81ae15ff2
madava jayasundara <clouddevopsworks@gmail.com>
2023-01-04 08:17:03 +0530

    remove userdata

--- asg/user-data/sandbox/1851-backend/user-data.sh
+++ asg/user-data/sandbox/1851-backend/user-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt-get install git -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install 9.0.0 -g
-npm install pm2@5.2.2 -g
+# apt-get install git -y
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# nvm install 9.0.0 -g
+# npm install pm2@5.2.2 -g
