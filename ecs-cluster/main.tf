7c49a632be8141e27be8a821dfbe853bfdb9ab2a
madava jayasundara <clouddevopsworks@gmail.com>
2023-01-14 19:26:52 +0530

    update capacity provider config

--- ecs-cluster/main.tf
+++ ecs-cluster/main.tf
@@ -8,6 +8,8 @@ resource "aws_ecs_cluster_capacity_providers" "capacity_providers" {
   capacity_providers = ["FARGATE"]
 
   default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
     capacity_provider = "FARGATE"
   }
 }
