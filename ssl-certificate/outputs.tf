678192acfac86ed1dab98401898030adc57ad808
madava jayasundara <clouddevopsworks@gmail.com>
2023-01-16 16:51:28 +0530

    change output

--- ssl-certificate/outputs.tf
+++ ssl-certificate/outputs.tf
@@ -6,14 +6,6 @@ output "cartificate_domain_name" {
   value = aws_acm_certificate.acm_certificate.domain_name
 }
 
-output "validation_record_name" {
-  value = aws_acm_certificate.acm_certificate.resource_record_name
-}
-
-output "validation_record_type" {
-  value = aws_acm_certificate.acm_certificate.resource_record_type
-}
-
-output "validation_record_value" {
-  value = aws_acm_certificate.acm_certificate.resource_record_value
+output "domain_validation_options" {
+  value = aws_acm_certificate.acm_certificate.domain_validation_options
 }
