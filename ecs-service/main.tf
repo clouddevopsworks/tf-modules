6a0e8c566054b4793af36c100e7af1ad706854cd
madava jayasundara <clouddevopsworks@gmail.com>
2023-01-17 14:33:26 +0530

    update ecs service

--- ecs-service/main.tf
+++ ecs-service/main.tf
@@ -5,6 +5,7 @@ resource "aws_ecs_service" "ecs_service" {
   desired_count          = var.desired_count
   force_new_deployment   = true
   enable_execute_command = true
+  launch_type            = "FARGATE"
   load_balancer {
     target_group_arn = var.target_group_arn
     container_name   = var.container_name
