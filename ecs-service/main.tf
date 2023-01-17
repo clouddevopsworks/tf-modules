c9c6bf6c5c276633049d08dfd86c5f8e5562b604
madava jayasundara <clouddevopsworks@gmail.com>
2023-01-17 13:54:24 +0530

    update es service

--- ecs-service/main.tf
+++ ecs-service/main.tf
@@ -24,7 +24,7 @@ resource "aws_ecs_service" "ecs_service" {
     subnets          = var.subnets
     assign_public_ip = var.assign_public_ip
   }
-  
+
   capacity_provider_strategy {
     capacity_provider = "FARGATE_SPOT"
     weight            = 100
@@ -35,7 +35,6 @@ resource "aws_ecs_service" "ecs_service" {
     ignore_changes = [
       load_balancer,
       desired_count,
-      task_definition,
       capacity_provider_strategy,
     ]
   }
